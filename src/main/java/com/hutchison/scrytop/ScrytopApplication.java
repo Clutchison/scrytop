2f5cea4ba370fbeb2f0d279ffd337dc746dc05c7
Sean Hutchison <xskhutchx@gmail.com>
2020-11-07 10:37:21 -0700

    first

---
+++ src/main/java/com/hutchison/scrytop/ScrytopApplication.java
@@ -0,0 +1,13 @@
+package com.hutchison.scrytop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrytopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrytopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hutchison/scrytop/ScrytopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutchison.scrytop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrytopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
